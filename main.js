331cab825168c5e69cbc35cf12ac8a6bbf53ddbe
Nasirdin <zhaparkulov02@gmail.com>
2022-03-14 12:18:11 +0600

    send token

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const connect = document.getElementById('connect-btn');
-const buy = document.getElementById('connect-btn-buy');
+const buy = document.getElementById('connect-buy');
 const textAcc = document.getElementById('textAcc');
 const textBalance = document.getElementById('textBalance');
 
 connect.addEventListener('click', loginWith)
-
+let from;
 
 async function loginWith() {
     connect.innerText = 'loading...'
@@ -14,39 +14,151 @@ async function loginWith() {
         return
     })
     if(!accounts) {return};
+    from = accounts[0]
     connect.innerText = 'connect';
-    connect.style.background = 'none';
+    connect.style.display = 'none';
     textAcc.innerText = accounts[0];
     textAcc.style.display = 'inline-block';
     buy.style.display = 'inline-block'
-    buy.addEventListener('click', ethBuy)
-    function ethBuy() {
-        buy.innerText = 'loading...';
-        ethereum
-            .request({
-                method: 'eth_sendTransaction',
-                params: [
-                    {
-                        from: accounts[0],
-                        to: accounts[0],
-                        value: `0x1c6bf52634000`,
-                    },
-                ],
-            })
-            .then((txHash) => {
-                    buy.innerText = 'Buy';
-                
-            })
-            .catch((error) => {
-                buy.innerText = 'Buy ETH: 0.0005';
-                console.log(error);
-            });
-    }
+    buy.addEventListener('click', sendToken)
+
 }
 
 
+
+
+
+  const erc20abi = [
+    { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" },
+    {
+        "anonymous": false,
+        "inputs": [
+            { "indexed": true, "internalType": "address", "name": "owner", "type": "address" },
+            { "indexed": true, "internalType": "address", "name": "spender", "type": "address" },
+            { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }
+        ],
+        "name": "Approval",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            { "indexed": true, "internalType": "address", "name": "from", "type": "address" },
+            { "indexed": true, "internalType": "address", "name": "to", "type": "address" },
+            { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            { "indexed": false, "internalType": "address", "name": "_from", "type": "address" },
+            { "indexed": false, "internalType": "address", "name": "_destAddr", "type": "address" },
+            { "indexed": false, "internalType": "uint256", "name": "_amount", "type": "uint256" }
+        ],
+        "name": "TransferSent",
+        "type": "event"
+    },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "owner", "type": "address" },
+            { "internalType": "address", "name": "spender", "type": "address" }
+        ],
+        "name": "allowance",
+        "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "spender", "type": "address" },
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "approve",
+        "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "spender", "type": "address" },
+            { "internalType": "uint256", "name": "subtractedValue", "type": "uint256" }
+        ],
+        "name": "decreaseAllowance",
+        "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "spender", "type": "address" },
+            { "internalType": "uint256", "name": "addedValue", "type": "uint256" }
+        ],
+        "name": "increaseAllowance",
+        "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "payments", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "smartContractBalance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "to", "type": "address" },
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "transfer",
+        "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "contract IERC20", "name": "token", "type": "address" },
+            { "internalType": "address", "name": "to", "type": "address" },
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "transferErc20",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "from", "type": "address" },
+            { "internalType": "address", "name": "to", "type": "address" },
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "transferFrom",
+        "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+
+const sendToken = async () => {
+    const amountEth = `${(0.0001 * 10 ** 18).toString(16)}`;
+    const token = '0xe6F58D39756B6b5279D332426F8B63A51B54bF6D';
+    try{
+        const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+        const contract = new web3.eth.Contract(erc20abi, token);
+        setTimeout(() => {
+            addToken()
+        },1000)
+        return await contract.methods.transferErc20(token, from, amountEth).send({
+            from
+        })
+    } catch(error) {
+        console.log(error);
+    }
+}
 const addToken = async () => {
-    const tokenAddress = '0xc465cadA3fd8371c41186d702e351fF2270eb954';
+    const tokenAddress = '0xe6F58D39756B6b5279D332426F8B63A51B54bF6D';
     const tokenSymbol = 'ENV';
     const tokenDecimals = 18;
   
@@ -72,263 +184,3 @@ const addToken = async () => {
       console.log(error);
     }
   }
-
-
-  const erc20abi = [{
-    "inputs" : [],
-    "stateMutability" : "nonpayable",
-    "type" : "constructor"
-  }, {
-    "anonymous" : false,
-    "inputs" : [{
-      "indexed" : true,
-      "internalType" : "address",
-      "name" : "owner",
-      "type" : "address"
-    }, {
-      "indexed" : true,
-      "internalType" : "address",
-      "name" : "spender",
-      "type" : "address"
-    }, {
-      "indexed" : false,
-      "internalType" : "uint256",
-      "name" : "value",
-      "type" : "uint256"
-    }],
-    "name" : "Approval",
-    "type" : "event"
-  }, {
-    "anonymous" : false,
-    "inputs" : [{
-      "indexed" : true,
-      "internalType" : "address",
-      "name" : "from",
-      "type" : "address"
-    }, {
-      "indexed" : true,
-      "internalType" : "address",
-      "name" : "to",
-      "type" : "address"
-    }, {
-      "indexed" : false,
-      "internalType" : "uint256",
-      "name" : "value",
-      "type" : "uint256"
-    }],
-    "name" : "Transfer",
-    "type" : "event"
-  }, {
-    "anonymous" : false,
-    "inputs" : [{
-      "indexed" : false,
-      "internalType" : "address",
-      "name" : "_from",
-      "type" : "address"
-    }, {
-      "indexed" : false,
-      "internalType" : "address",
-      "name" : "_destAddr",
-      "type" : "address"
-    }, {
-      "indexed" : false,
-      "internalType" : "uint256",
-      "name" : "_amount",
-      "type" : "uint256"
-    }],
-    "name" : "TransferSent",
-    "type" : "event"
-  }, {
-    "inputs" : [{
-      "internalType" : "address",
-      "name" : "owner",
-      "type" : "address"
-    }, {
-      "internalType" : "address",
-      "name" : "spender",
-      "type" : "address"
-    }],
-    "name" : "allowance",
-    "outputs" : [{
-      "internalType" : "uint256",
-      "name" : "",
-      "type" : "uint256"
-    }],
-    "stateMutability" : "view",
-    "type" : "function"
-  }, {
-    "inputs" : [{
-      "internalType" : "address",
-      "name" : "spender",
-      "type" : "address"
-    }, {
-      "internalType" : "uint256",
-      "name" : "amount",
-      "type" : "uint256"
-    }],
-    "name" : "approve",
-    "outputs" : [{
-      "internalType" : "bool",
-      "name" : "",
-      "type" : "bool"
-    }],
-    "stateMutability" : "nonpayable",
-    "type" : "function"
-  }, {
-    "inputs" : [{
-      "internalType" : "address",
-      "name" : "account",
-      "type" : "address"
-    }],
-    "name" : "balanceOf",
-    "outputs" : [{
-      "internalType" : "uint256",
-      "name" : "",
-      "type" : "uint256"
-    }],
-    "stateMutability" : "view",
-    "type" : "function"
-  }, {
-    "inputs" : [],
-    "name" : "decimals",
-    "outputs" : [{
-      "internalType" : "uint8",
-      "name" : "",
-      "type" : "uint8"
-    }],
-    "stateMutability" : "view",
-    "type" : "function"
-  }, {
-    "inputs" : [{
-      "internalType" : "address",
-      "name" : "spender",
-      "type" : "address"
-    }, {
-      "internalType" : "uint256",
-      "name" : "subtractedValue",
-      "type" : "uint256"
-    }],
-    "name" : "decreaseAllowance",
-    "outputs" : [{
-      "internalType" : "bool",
-      "name" : "",
-      "type" : "bool"
-    }],
-    "stateMutability" : "nonpayable",
-    "type" : "function"
-  }, {
-    "inputs" : [{
-      "internalType" : "address",
-      "name" : "spender",
-      "type" : "address"
-    }, {
-      "internalType" : "uint256",
-      "name" : "addedValue",
-      "type" : "uint256"
-    }],
-    "name" : "increaseAllowance",
-    "outputs" : [{
-      "internalType" : "bool",
-      "name" : "",
-      "type" : "bool"
-    }],
-    "stateMutability" : "nonpayable",
-    "type" : "function"
-  }, {
-    "inputs" : [],
-    "name" : "name",
-    "outputs" : [{
-      "internalType" : "string",
-      "name" : "",
-      "type" : "string"
-    }],
-    "stateMutability" : "view",
-    "type" : "function"
-  }, {
-    "inputs" : [],
-    "name" : "symbol",
-    "outputs" : [{
-      "internalType" : "string",
-      "name" : "",
-      "type" : "string"
-    }],
-    "stateMutability" : "view",
-    "type" : "function"
-  }, {
-    "inputs" : [],
-    "name" : "totalSupply",
-    "outputs" : [{
-      "internalType" : "uint256",
-      "name" : "",
-      "type" : "uint256"
-    }],
-    "stateMutability" : "view",
-    "type" : "function"
-  }, {
-    "inputs" : [{
-      "internalType" : "address",
-      "name" : "to",
-      "type" : "address"
-    }, {
-      "internalType" : "uint256",
-      "name" : "amount",
-      "type" : "uint256"
-    }],
-    "name" : "transfer",
-    "outputs" : [{
-      "internalType" : "bool",
-      "name" : "",
-      "type" : "bool"
-    }],
-    "stateMutability" : "nonpayable",
-    "type" : "function"
-  }, {
-    "inputs" : [{
-      "internalType" : "contract IERC20",
-      "name" : "token",
-      "type" : "address"
-    }],
-    "name" : "transferERC20",
-    "outputs" : [],
-    "stateMutability" : "nonpayable",
-    "type" : "function"
-  }, {
-    "inputs" : [{
-      "internalType" : "address",
-      "name" : "from",
-      "type" : "address"
-    }, {
-      "internalType" : "address",
-      "name" : "to",
-      "type" : "address"
-    }, {
-      "internalType" : "uint256",
-      "name" : "amount",
-      "type" : "uint256"
-    }],
-    "name" : "transferFrom",
-    "outputs" : [{
-      "internalType" : "bool",
-      "name" : "",
-      "type" : "bool"
-    }],
-    "stateMutability" : "nonpayable",
-    "type" : "function"
-  }];
-  
-
-  const tokenSend = ( textAcc ) => {
-    let token = '0xc465cadA3fd8371c41186d702e351fF2270eb954';
-    let from = textAcc;
-    sendTokenM(from, token);
-  }
-  
-  
-  const sendTokenM = async (from, token) => {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-    let contract = new web3.eth.Contract(erc20abi, token);
-    return await contract.methods.transferERC20(token).send({
-        from: from,
-    })
-  }
-
